df8ea5a672a13105f76928896994dc163f4200a5
Jesus Villota <danielvillota1016@gmail.com>
2019-09-15 17:37:34 -0500

    Interface and Class of HashTable with chaining method for solving collisions were added

---
+++ src/model/HNode.java
@@ -0,0 +1,38 @@
+package model;
+
+public class HNode<K,V> {
+		private K key;
+		private V value;
+		
+		private HNode<K,V> next;
+		private HNode<K,V> previous;
+		
+		public HNode(K key, V value){
+			this.setKey(key);
+			this.setValue(value);
+		}
+		public K getKey() {
+			return key;
+		}
+		public void setKey(K key) {
+			this.key = key;
+		}
+		public V getValue() {
+			return value;
+		}
+		public void setValue(V value) {
+			this.value = value;
+		}
+		public HNode<K,V> getNext() {
+			return next;
+		}
+		public void setNext(HNode<K,V> next) {
+			this.next = next;
+		}
+		public HNode<K,V> getPrevious() {
+			return previous;
+		}
+		public void setPrevious(HNode<K,V> previous) {
+			this.previous = previous;
+		}
+}
---
+++ src/model/HashTableInterface.java
@@ -0,0 +1,9 @@
+package model;
+
+public interface HashTableInterface<K,V> {
+	V search(K key);
+	void insert(K key, V value);
+	void delete(K key);
+	boolean contains(K key);
+	
+}
